dba32663f97d3ad0099750cf6659890613a09aa5
lununez <lununez@me.com>
2024-10-11 09:37:12 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import RiskMatrix from './components/RiskMatrix';
+
+function App() {
+  return (
+    <div className="App">
+      <RiskMatrix />
+    </div>
+  );
+}
+
+export default App;
